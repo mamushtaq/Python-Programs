3fe1fd2e9b7cb160b499ac1b14b5df764c851eee
Ali <alimushtaq403@gmail.com>
2021-08-13 21:49:15 +0500

    stored all pi digits in a string

--- Python_crash_course/files_and_exceptions/file_reader.py
+++ Python_crash_course/files_and_exceptions/file_reader.py
@@ -2,3 +2,11 @@ with open("py_digits.txt") as file_object:
     lines = file_object.readlines()
     for line in lines:
         print(line.rstrip())
+
+pi_digits = ""
+
+for line in lines:
+    pi_digits += line.strip()
+
+print(pi_digits)
+print(len(pi_digits))
