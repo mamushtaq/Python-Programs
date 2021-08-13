0a1ce1b508661295b45c6039be2261613b2d037f
Ali <alimushtaq403@gmail.com>
2021-08-13 22:21:30 +0500

    Birthday in first 1000 pi digits

--- Python_crash_course/files_and_exceptions/file_reader.py
+++ Python_crash_course/files_and_exceptions/file_reader.py
@@ -1,12 +1,13 @@
 with open("py_digits.txt") as file_object:
     lines = file_object.readlines()
-    for line in lines:
-        print(line.rstrip())
 
 pi_digits = ""
 
 for line in lines:
     pi_digits += line.strip()
 
-print(pi_digits)
-print(len(pi_digits))
+birthday = input("Enter your birthday in the format mmddyyyy: ")
+if birthday in pi_digits:
+    print("Your birthday is in the first 1000 digits of pi")
+else:
+    print("Your birthday is not in first 1000 pi_digits")
