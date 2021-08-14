5a7d2cf7ec9623432176bf96a76e6885ac644b35
Ali <alimushtaq403@gmail.com>
2021-08-14 14:45:11 +0500

    created a division program using try except

--- Python_crash_course/files_and_exceptions/exceptions/division.py
+++ Python_crash_course/files_and_exceptions/exceptions/division.py
@@ -1,4 +1,12 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("Integers cannot be divided by zero")
+print("I will divide two numbers for you.\nEnter 'q' to quit")
+while True:
+    first_number = input("\nEnter first number : ")
+    if first_number == 'q':
+        break
+    second_number = input("\nEnter second number : ")
+    if second_number == 'q':
+        break
+    try:
+        print(int(first_number)/int(second_number))
+    except ZeroDivisionError:
+        print("\nCannot be divided by zero")
