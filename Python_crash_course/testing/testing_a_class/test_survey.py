eefbe2eb24cb2d0daa95ac696a779f52047bed8d
Ali <alimushtaq403@gmail.com>
2021-08-15 20:09:39 +0500

    Testing using setup method

--- Python_crash_course/testing/testing_a_class/test_survey.py
+++ Python_crash_course/testing/testing_a_class/test_survey.py
@@ -4,23 +4,25 @@ from survey import AnonymousSurvey
 class TestingAnonymousSurvey(unittest.TestCase):
     """It tests the survey.py"""
 
+    def setUp(self):
+        """Creates sets of responses to be tested"""
+        question = "Which language did you learn to speak first"
+        self.mysurvey = AnonymousSurvey(question)
+        self.responses = ['English','Urdu', 'Spanish','Japanese','German']
+
     def test_store_single_response(self):
         """Tests if single response is stored in the list"""
-        question = "Which language did you learn to speak first"
-        mysurvey = AnonymousSurvey(question)
-        mysurvey.store_response('English')
+        
+        self.mysurvey.store_response(self.responses[0])
 
-        self.assertIn('English', mysurvey.response)
+        self.assertIn(self.responses[0], self.mysurvey.response)
 
     def test_store_three_responses(self):
         """Tests if three responses are stored in the list"""
-        question = "Which language did you learn to speak first"
-        mysurvey = AnonymousSurvey(question)
-        responses = ['Urdu', 'Spanish','Japanese','German']
-        for response in responses:
-            mysurvey.store_response(response)
-        for response in responses:
-            self.assertIn(response, mysurvey.response)
+        for response in self.responses[1:4]:
+            self.mysurvey.store_response(response)
+        for response in self.responses[1:4]:
+            self.assertIn(response, self.mysurvey.response)
 
 
 unittest.main()
