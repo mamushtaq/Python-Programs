812be8dfc78e8da1f48aeb3a3fa54e17b29e7942
Ali <alimushtaq403@gmail.com>
2021-08-15 20:02:34 +0500

    Checking if three responses are stored without problem

--- Python_crash_course/testing/testing_a_class/test_survey.py
+++ Python_crash_course/testing/testing_a_class/test_survey.py
@@ -12,4 +12,15 @@ class TestingAnonymousSurvey(unittest.TestCase):
 
         self.assertIn('English', mysurvey.response)
 
+    def test_store_three_responses(self):
+        """Tests if three responses are stored in the list"""
+        question = "Which language did you learn to speak first"
+        mysurvey = AnonymousSurvey(question)
+        responses = ['Urdu', 'Spanish','Japanese','German']
+        for response in responses:
+            mysurvey.store_response(response)
+        for response in responses:
+            self.assertIn(response, mysurvey.response)
+
+
 unittest.main()
