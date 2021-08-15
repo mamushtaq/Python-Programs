8b29c73b3607531571c8b6bd65c5787b31ef460f
Ali <alimushtaq403@gmail.com>
2021-08-15 12:20:54 +0500

    used load

--- Python_crash_course/files_and_exceptions/JSON/numbers.py
+++ Python_crash_course/files_and_exceptions/JSON/numbers.py
@@ -1,8 +1,8 @@
 import json
 
-numbers = [2, 3, 4, 5, 6]
-
 filename = 'numbers.json'
 
-with open(filename, 'w') as f_obj:
-    json.dump(numbers, f_obj)
+with open(filename) as f_obj:
+    numbers = json.load(f_obj)
+
+print(numbers)
